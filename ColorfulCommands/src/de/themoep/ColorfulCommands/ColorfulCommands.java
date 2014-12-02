f289048faf4c787024ac250881c8f7a48380015c
Max Lee <Phoenix616@users.noreply.github.com>
2014-12-03 00:35:46 +0100

    Made code shorter and probably more performant

--- ColorfulCommands/src/de/themoep/ColorfulCommands/ColorfulCommands.java
+++ ColorfulCommands/src/de/themoep/ColorfulCommands/ColorfulCommands.java
@@ -6,18 +6,17 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class ColorfulCommands extends JavaPlugin {
-	
-	@Override	
+
+	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
 		if(args.length < 2)
 			sender.sendMessage("Usage: /" + cmd.getName() + " <command> <command arguments>");
 		else {
 			String cmdstr = args[0].replaceFirst("^/", "");
-			for(int i = 1; i < args.length; i++) {
-				cmdstr += " " + ChatColor.translateAlternateColorCodes('&', args[i]);
-			}
-			this.getServer().dispatchCommand(sender, cmdstr);
+			for(int i = 1; i < args.length; i++)
+				cmdstr += " " + args[i];
+			this.getServer().dispatchCommand(sender, ChatColor.translateAlternateColorCodes('&', cmdstr));
 		}
-		return true;		
+		return true;
 	}
 }
