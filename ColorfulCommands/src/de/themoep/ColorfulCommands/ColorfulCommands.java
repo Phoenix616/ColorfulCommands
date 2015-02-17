6c3edaf59970d52075335cae2f5928e7e34645b5
Max Lee <Phoenix616@users.noreply.github.com>
2015-02-18 00:20:37 +0100

    Made code even shorter.

--- ColorfulCommands/src/de/themoep/ColorfulCommands/ColorfulCommands.java
+++ ColorfulCommands/src/de/themoep/ColorfulCommands/ColorfulCommands.java
@@ -10,13 +10,11 @@ public class ColorfulCommands extends JavaPlugin {
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
 		if(args.length < 2)
-			sender.sendMessage("Usage: /" + cmd.getName() + " <command> <command arguments>");
-		else {
-			String cmdstr = args[0].replaceFirst("^/", "");
-			for(int i = 1; i < args.length; i++)
-				cmdstr += " " + args[i];
-			this.getServer().dispatchCommand(sender, ChatColor.translateAlternateColorCodes('&', cmdstr));
-		}
+            return false;
+        String cmdstr = args[0].replaceFirst("^/", "");
+        for(int i = 1; i < args.length; i++)
+            cmdstr += " " + args[i];
+        this.getServer().dispatchCommand(sender, ChatColor.translateAlternateColorCodes('&', cmdstr));
 		return true;
 	}
 }
